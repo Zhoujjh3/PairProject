44c9bdd7ede3260b6eddd346b7a240d6cdd38dcb
Justin Zhou <justzh2016@gmail.com>
2023-03-22 10:02:32 -0400

    Added setup classes

---
+++ PairProject/src/Chamber.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class Chamber extends JPanel{
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+}
--- PairProject/src/RunGame.java
+++ PairProject/src/RunGame.java
@@ -1,9 +1,34 @@
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 public class RunGame {
+	
+	JFrame frame;
+	JPanel chamber;
+	
+	public RunGame() {
+		frame = new JFrame();
+		chamber = new Chamber();
+		chamber.setPreferredSize(new Dimension(1000, 750));
+		chamber.addMouseListener(new Clicker());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(chamber);
+		frame.pack();
+	    frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("mad cap");
+		new RunGame();
 	}
 	
 
