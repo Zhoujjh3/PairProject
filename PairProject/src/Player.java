add7ba11fc1eb2d7930e6608ad6df048559d88d0
Justin Zhou <justzh2016@gmail.com>
2023-03-30 09:46:14 -0400

    unfinished changes to player

--- PairProject/src/Player.java
+++ PairProject/src/Player.java
@@ -9,6 +9,7 @@ public class Player {
 	int runCounter, swingCounter;
 	double velocityX, velocityY, accelX, accelY;
 	int xPos, yPos;
+	boolean direction;
 	Image player;
 	
 	enum States{
@@ -26,12 +27,18 @@ public class Player {
 		player = new ImageIcon("images//swordsman3.png").getImage();
 		xPos = 500;
 		yPos = 500;
+		
+		velocityX = 0;
+		velocityY = 0;
+		accelX = 0;
+		accelY = 0;
 	}
 	
 	public void updatePlayer() {
 		switch(state) {
 		case IDLE:
-			
+			velocityY = 0;
+			velocityX = 0;
 			break;
 		case RUNNING:
 			
@@ -43,6 +50,26 @@ public class Player {
 			
 			break;
 		}
+	}
+	
+	public void changeDirection(String dir) {
+		if(dir.equals("LEFT")) {
+			direction = false;
+		} else if(dir.equals("RIGHT")){
+			direction = true;
+		}
+	}
+	
+	public int getXPos() {
+		return xPos;
+	}
+	
+	public int getYPos() {
+		return yPos;
+	}
+	
+	public void changeState(States state) {
+		this.state = state;
 	}
 	
 	public void drawPlayer(Graphics g) {
