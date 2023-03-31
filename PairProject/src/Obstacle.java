7c408740b4ec7a0abb4af22a35bb886e29b17076
maxbalkirsky <maxbalkirsky@MacBookAir7a2dec2e>
2023-03-31 08:51:37 -0400

    Obstacle superclass

--- PairProject/src/Obstacle.java
+++ PairProject/src/Obstacle.java
@@ -1,4 +1,17 @@
+import java.awt.*;
 
 public abstract class Obstacle {
 
+	int xPos, yPos;
+	Image obstacle;
+	
+	public int getXPos() {return xPos;}
+	
+	public int getYPos() {return yPos;}
+	
+	public void drawObstacle(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(obstacle, xPos, yPos, 200, 200, null);
+	}
+	
 }
