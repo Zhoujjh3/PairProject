50693fc284dcb13a4209cc4f96a30295436a7b7e
maxbalkirsky <maxbalkirsky@MacBookAir7a2dec2e>
2023-05-02 09:35:43 -0400

    Cannonball functionality done.

--- PairProject/src/CannonBall.java
+++ PairProject/src/CannonBall.java
@@ -4,10 +4,18 @@ public class CannonBall extends Projectile {
 
 	CannonBall(int x, int y, int playerXPos, int playerYPos) {
 		super(x,y);
+		int playerXMiddle = playerXPos + 93;
+		int playerYMiddle = playerYPos + 67;
 		setColor(Color.red);
-		
-		setXVelocity(Math.atan((playerXPos + 93)/(playerYPos + 67))); //((playerXPos + 93) - x)/100.0);
-		setYVelocity(Math.atan((playerXPos + 93)/(playerYPos + 67)));
+		double theta = Math.atan((double) (Math.abs(playerXMiddle - x))/(double) (Math.abs(playerYMiddle - y)));
+		double direction;
+		if((playerXMiddle - x) >= 0) {
+			direction = 1.0;
+		} else {
+			direction = -1.0;
+		}
+		setXVelocity(4.0*Math.sin(theta) * (direction));
+		setYVelocity(4.0*Math.cos(theta));
 		//setXVelocity(((playerXPos + 93) - x)/100.0);
 		//setYVelocity(((playerYPos + 67) - y)/100.0);
 	}
